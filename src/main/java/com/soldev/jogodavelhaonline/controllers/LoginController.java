468065d40e91fa59c4cd87ff7b9f14a3d71e9362
Yan Gorski <gorski.yan.93@gmail.com>
2023-10-08 18:09:09 -0300

    adicionada a página de Lobby

--- src/main/java/com/soldev/jogodavelhaonline/controllers/LoginController.java
+++ src/main/java/com/soldev/jogodavelhaonline/controllers/LoginController.java
@@ -3,9 +3,20 @@ package com.soldev.jogodavelhaonline.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+import com.soldev.jogodavelhaonline.models.Usuario;
+import com.soldev.jogodavelhaonline.repositories.UsuarioRepository;
 
 @Controller
 public class LoginController {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public LoginController(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
 
     @GetMapping("/login")
     public String loginPage() {
@@ -13,8 +24,17 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String loginSubmit() {
-        // Adicione aqui a lógica de autenticação do usuário
-        return "redirect:/dashboard"; // Redireciona para a página de dashboard após o login bem-sucedido
+    public String login(@RequestParam String username, @RequestParam String password, Model model) {
+
+        Usuario usuario = usuarioRepository.findByUsername(username);
+
+        if (usuario != null && usuario.getPassword().equals(password)) {
+            // Login bem-sucedido, redirecione para a página do lobby
+            return "redirect:/lobby";
+        } else {
+            // Login falhou, adicione uma mensagem de erro ao modelo
+            model.addAttribute("error", "Usuário não cadastrado ou senha incorreta");
+            return "login";
+        }
     }
 }
