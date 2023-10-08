f21454afce31041bf54208b0c48b2b124f286681
Yan Gorski <gorski.yan.93@gmail.com>
2023-10-08 02:35:11 -0300

    commit inicial

---
+++ src/main/java/com/soldev/jogodavelhaonline/JogoDaVelhaOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.soldev.jogodavelhaonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogoDaVelhaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogoDaVelhaOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soldev/jogodavelhaonline/JogoDaVelhaOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soldev.jogodavelhaonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogoDaVelhaOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
