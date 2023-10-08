6793a0738a7dab6576fee15ece46c57cb7d6e049
Yan Gorski <gorski.yan.93@gmail.com>
2023-10-08 03:56:38 -0300

    testando outra coisa

--- src/main/java/com/soldev/jogodavelhaonline/JogoDaVelhaOnlineApplication.java
+++ src/main/java/com/soldev/jogodavelhaonline/JogoDaVelhaOnlineApplication.java
@@ -3,7 +3,7 @@ package com.soldev.jogodavelhaonline;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.soldev.jogodavelhaonline")
 public class JogoDaVelhaOnlineApplication {
 
 	public static void main(String[] args) {
