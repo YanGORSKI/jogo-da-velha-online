80868f30a1c85f3b5b73a41dcb36ecf2c6357273
Yan Gorski <gorski.yan.93@gmail.com>
2023-10-08 16:21:48 -0300

    alterada a pagina inicial para pagina de login e cadastro

---
+++ src/main/java/com/soldev/jogodavelhaonline/UsuarioController.java
@@ -0,0 +1,18 @@
+package com.soldev.jogodavelhaonline;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UsuarioController {
+    @GetMapping("/login")
+    public String loginPage() {
+        // Lógica para login
+        return "index";
+    }
+
+    @GetMapping("/cadastro")
+    public String cadastroPage() {
+        return "cadastro.html"; // Esta é a página de cadastro que você criará mais tarde.
+    }
+}
