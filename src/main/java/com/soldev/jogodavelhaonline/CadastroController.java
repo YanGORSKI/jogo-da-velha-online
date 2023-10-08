0352968ba62c5abf4ec8455d218b15455b83df25
Yan Gorski <gorski.yan.93@gmail.com>
2023-10-08 16:33:41 -0300

    separados os controllers

---
+++ src/main/java/com/soldev/jogodavelhaonline/CadastroController.java
@@ -0,0 +1,20 @@
+package com.soldev.jogodavelhaonline;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CadastroController {
+
+    @GetMapping("/cadastro")
+    public String cadastroPage() {
+        return "cadastro";
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrarUsuario() {
+        // Adicione aqui a lógica para criar um novo usuário no banco de dados
+        return "redirect:/login"; // Redireciona de volta para a página de login após o cadastro bem-sucedido
+    }
+}
---
+++ src/main/java/com/soldev/jogodavelhaonline/LoginController.java
@@ -0,0 +1,20 @@
+package com.soldev.jogodavelhaonline;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "index";
+    }
+
+    @PostMapping("/login")
+    public String loginSubmit() {
+        // Adicione aqui a lógica de autenticação do usuário
+        return "redirect:/dashboard"; // Redireciona para a página de dashboard após o login bem-sucedido
+    }
+}
