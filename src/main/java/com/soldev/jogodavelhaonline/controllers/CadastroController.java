517e2f65faab35248148ca5f6466800f77251f11
Yan Gorski <gorski.yan.93@gmail.com>
2023-10-08 16:51:33 -0300

    teste com package controllers

--- src/main/java/com/soldev/jogodavelhaonline/controllers/CadastroController.java
+++ src/main/java/com/soldev/jogodavelhaonline/controllers/CadastroController.java
@@ -1,4 +1,4 @@
-package com.soldev.jogodavelhaonline;
+package com.soldev.jogodavelhaonline.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
